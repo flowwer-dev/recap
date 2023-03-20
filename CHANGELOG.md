f6c3880d21f8fa2db45ab5a184cfa7bb490157af
Manuel de la Torre <manuelmhtr@gmail.com>
2023-03-20 11:15:13 -0600

    First version launch. With 💙 from Guadalajara, MX.

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [1.0.0] - 2023-03-19
+### Added
+- First launch, with ♥ from Guadalajara, Mexico.
--- src/services/telemetry/sendSuccess.js
+++ src/services/telemetry/sendSuccess.js
@@ -5,8 +5,9 @@ module.exports = ({
 }) => {
   const timeSec = Math.floor(timeMs / 1000);
   const timeMin = Math.floor(timeMs / 60000);
-  const contentLength = results.content?.length;
-  const contentLines = results.content?.split('\n').length;
+  const content = results.content || '';
+  const contentLength = content.length;
+  const contentLines = content.split('\n').length;
 
   tracker.track('success', {
     // Necessary to learn how long it takes to run the action:
